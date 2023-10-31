2f4f9368de1aad7076f98a52875c1bb3ce78a3e6
nfpranta <nfpranta@gmail.com>
2023-10-31 06:27:20 +0600

    Added a new solution of daily challenge

---
+++ LeetCode2433.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+  vector<int> findArray(vector<int> &pref)
+  {
+    vector<int> ans;
+    ans.push_back(pref[0]);
+    int n = pref.size();
+    for (int i = 1; i < n; i++)
+    {
+      ans.push_back(pref[i] ^ pref[i - 1]);
+    }
+    return ans;
+  }
+};
