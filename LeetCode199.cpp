7699f40a969fa81f328e6a835868c1f11aa19f6b
nfpranta <nfpranta@gmail.com>
2024-03-03 23:28:41 +0600

    Solved BFS problem

---
+++ LeetCode199.cpp
@@ -0,0 +1,45 @@
+/**
+ *Definition for a binary tree node.
+ *struct TreeNode {
+ *int val;
+ *TreeNode * left;
+ *TreeNode * right;
+ *TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *};
+ */
+class Solution
+{
+public:
+    vector<int> rightSideView(TreeNode *root)
+    {
+        vector<int> ans;
+        if (root == NULL)
+            return ans;
+        queue<TreeNode *> q;
+        q.push(root);
+        while (!q.empty())
+        {
+            int sz = q.size();
+            vector<int> data;
+            while (sz)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                data.push_back(node->val);
+                if (node->left)
+                {
+                    q.push(node->left);
+                }
+                if (node->right)
+                {
+                    q.push(node->right);
+                }
+                sz--;
+            }
+            ans.push_back(data.back());
+        }
+        return ans;
+    }
+};
